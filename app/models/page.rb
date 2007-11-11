c06aaba80cf2113e9dd73ee8d5eb058fd7d4568a
andrew <andrew@0f7b21a7-9e3a-4941-bbeb-ce5c7c368fa7>
2007-11-11 06:19:43 +0000

    better regular expression that actually works
    git-svn-id: http://svn.barleysodas.com/barleysodas/trunk@17 0f7b21a7-9e3a-4941-bbeb-ce5c7c368fa7

--- app/models/page.rb
+++ app/models/page.rb
@@ -16,7 +16,7 @@ class Page < ActiveRecord::Base
   belongs_to :owner, :polymorphic => true
   validates_presence_of :title
   validates_uniqueness_of :title, :scope => 'owner_type'
-  validates_format_of :title, :with => /[A-Za-z0-9 ]/,
+  validates_format_of :title, :with => /^([A-Za-z0-9 ])+$/,
     :message => 'may only contain letters, numbers and spaces'
   before_save :update_html
   
