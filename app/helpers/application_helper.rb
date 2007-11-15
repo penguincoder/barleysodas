21ef68515570db14af74505b61349c5136eb9198
andrew <andrew@0f7b21a7-9e3a-4941-bbeb-ce5c7c368fa7>
2007-11-15 06:49:33 +0000

    base layout
    git-svn-id: http://svn.barleysodas.com/barleysodas/trunk@24 0f7b21a7-9e3a-4941-bbeb-ce5c7c368fa7

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,8 +3,20 @@ module ApplicationHelper
   # Returns the title for a page. This could be a Page title or something else.
   #
   def page_title
+    return @page_title if @page_title
     return @page.title if @page
     "BarleySodas :: #{controller_class_name.gsub(/Controller/, '')}"
+  end
+  
+  ##
+  # Returns a secondary title for a page. Returns @secondary_title or the
+  # action in the controller.
+  #
+  def secondary_title
+    return @secondary_title if @secondary_title
+    return params[:action].to_s.capitalize.gsub(/_/) do |x|
+      $1.capitalize
+    end
   end
   
   ##
