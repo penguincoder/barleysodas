abcc827626289b11a2f26a861ab299b21c4fd857
andrew <andrew@0f7b21a7-9e3a-4941-bbeb-ce5c7c368fa7>
2007-11-17 07:14:26 +0000

    adding autocomplete controller and tests
    git-svn-id: http://svn.barleysodas.com/barleysodas/trunk@36 0f7b21a7-9e3a-4941-bbeb-ce5c7c368fa7

---
+++ app/controllers/autocomplete_controller.rb
@@ -0,0 +1,27 @@
+class AutocompleteController < ApplicationController
+  append_before_filter :ensure_xhr
+  
+  ##
+  # Automatically finds and returns a nice list of things. It is stupid in that
+  # it only finds the first thing available, but it will find it all.
+  #
+  # It expects to have parameters passed in the form:
+  #
+  #   'brewery' => { 'name' => 'foo' }
+  #
+  # Only one of those sets. If this is done, then all will be peachy.
+  #
+  def index
+    key = params.keys.detect do |x|
+      x.to_s != 'action' and x.to_s != 'controller'
+    end
+    render :nothing => true, :status => 500 if key.nil?
+    @value = params[key].keys.first
+    class_name = key.camelize.constantize
+    render :nothing => true unless class_name.new.respond_to?(@value)
+    @items = class_name.find(:all, :order => "#{@value} ASC", :select => @value,
+      :conditions => [ "lower(#{@value}) LIKE ?",
+        "%#{params[key][@value].downcase}%" ])
+    render :partial => 'autocomplete/results'
+  end
+end
